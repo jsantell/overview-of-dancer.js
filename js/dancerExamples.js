ae0691a5e99c2759c174572e8744573ff6fc301a
Jordan Santell <jsantell@gmail.com>
2012-07-16 22:15:04 -0400

    Init

---
+++ js/dancerExamples.js
@@ -0,0 +1,61 @@
+(function () {
+
+    /* Waveform */
+    var dancerWave = new Dancer('songs/zircon_devils_spirit', [ 'ogg', 'mp3' ]);
+
+    dancerWave.waveform( document.getElementById('waveform'), {
+        strokeStyle: '#666',
+        strokeWidth: 2
+    });
+
+    document.getElementById('waveform-btn').addEventListener( 'click', function () {
+        dancerWave.isLoaded() && !dancerWave.isPlaying() ?
+            dancerWave.play() :
+            dancerWave.stop() ;
+    });
+
+
+
+    /* FFT */
+    var dancerFFT = new Dancer('songs/zircon_devils_spirit', [ 'ogg', 'mp3' ]);
+
+    dancerFFT.fft( document.getElementById('fft'), {
+        fillStyle: '#666',
+        width: 2
+    });
+
+    document.getElementById('fft-btn').addEventListener( 'click', function () {
+        dancerFFT.isLoaded() && !dancerFFT.isPlaying() ?
+            dancerFFT.play() :
+            dancerFFT.stop() ;
+    });
+
+    /* Beat */
+    var
+      beatCanvas = document.getElementById('beat'),
+      beatCtx = beatCanvas.getContext('2d'),
+      dancerBeat = new Dancer('songs/anosou_ffvi', [ 'ogg', 'mp3' ]), beat;
+
+    beat = dancerBeat.createBeat({
+        frequency: [ 0, 10 ],
+        threshold: 0.25,
+        decay: 0.03,
+        onBeat: function () {
+            beatCtx.fillStyle = '#ff0077';
+        },
+        offBeat: function () {
+            beatCtx.fillStyle = '#666';
+        }
+    }).on();
+    
+    dancerBeat.fft( beatCanvas, {
+        fillStyle: '#666',
+        width: 10
+    });
+
+    document.getElementById('beat-btn').addEventListener( 'click', function () {
+        dancerBeat.isLoaded() && !dancerBeat.isPlaying() ?
+            dancerBeat.play() :
+            dancerBeat.stop() ;
+    });
+})();
